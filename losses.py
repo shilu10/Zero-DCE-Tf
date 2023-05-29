665e105505dd5457b039dde75a60d44d66a9d74d
adminuser <adminuser@kali.mvm>
2023-05-29 18:54:09 +0530

    added spatial consistency loss

--- losses.py
+++ losses.py
@@ -56,3 +56,70 @@ class IlluminationSmothnessLoss(keras.losses.Loss):
         count_h = tf.cast(count_h, dtype=tf.float32)
         count_w = tf.cast(count_w, dtype=tf.float32)
         return 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+
+
+
+class SpatialConsistencyLoss(keras.losses.Loss):
+    def __init__(self):
+        super(SpatialConsistencyLoss, self).__init__()
+    
+    def init_f_left(self, shape, dtype=None):
+        ker = np.zeros(shape, dtype=None)
+        ker[1][0] = -1
+        ker[1][1] = 1
+        return ker
+
+    def init_f_right(self, shape, dtype=None):
+        ker = np.zeros(shape, dtype=None)
+        ker[1][2] = -1
+        ker[1][1] = 1
+        return ker
+
+    def init_f_up(self, shape, dtype=None):
+        ker = np.zeros(shape, dtype=None)
+        ker[0][1] = -1
+        ker[1][1] = 1
+        return ker
+
+    def init_f_down(self, shape, dtype=None):
+        ker = np.zeros(shape, dtype=None)
+        ker[2][1] = -1
+        ker[1][1] = 1
+        return ker
+    
+    
+    def call(self, orginal, enchanced): 
+        orginal_mean = tf.reduce_mean(orginal, 3, keepdims=True)
+        enhanced_mean = tf.reduce_mean(enchanced, 3, keepdims=True)
+
+        original_pool =  AveragePooling2D(pool_size=4)(orginal_mean)		
+        enhanced_pool = AveragePooling2D(pool_size=4)(enhanced_mean)
+        
+        # original
+        d_original_left = Conv2D(kernel_initializer=self.init_f_left, 
+                                padding='same', kernel_size=(3,3), filters=1)(original_pool)
+        d_original_right = Conv2D(kernel_initializer=self.init_f_right,
+                                padding='same', kernel_size=(3,3), filters=1)(original_pool)
+        d_original_up = Conv2D(kernel_initializer=self.init_f_up,
+                                padding='same', kernel_size=(3,3), filters=1)(original_pool)
+        d_original_down = Conv2D(kernel_initializer=self.init_f_down,
+                                padding='same', kernel_size=(3,3), filters=1)(original_pool)
+        
+        # enchanced
+        d_enhanced_letf = Conv2D(kernel_initializer=self.init_f_left,
+                                padding='same', kernel_size=(3,3), filters=1)(enhanced_pool)
+        d_enhanced_right = Conv2D(kernel_initializer=self.init_f_right,
+                                padding='same', kernel_size=(3,3), filters=1)(enhanced_pool)
+        d_enhanced_up = Conv2D(kernel_initializer=self.init_f_up,
+                                padding='same', kernel_size=(3,3), filters=1)(enhanced_pool)
+        d_enhanced_down = Conv2D(kernel_initializer=self.init_f_down,
+                                padding='same', kernel_size=(3,3), filters=1)(enhanced_pool)
+
+        d_left = tf.pow(d_original_left - d_enhanced_letf, 2)
+        d_right = tf.pow(d_original_right - d_enhanced_right, 2)
+        d_up = tf.pow(d_original_up - d_enhanced_up, 2)
+        d_down = tf.pow(d_original_down - d_enhanced_down, 2)
+        E = (d_left + d_right + d_up + d_down)
+
+        return E
