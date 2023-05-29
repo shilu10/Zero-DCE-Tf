920a11c04f3cf8141376031de0fb70dfecfd04a1
adminuser <adminuser@kali.mvm>
2023-05-29 17:47:12 +0530

    added zerodcenet functional model

--- model.py
+++ model.py
@@ -55,3 +55,43 @@ class ZeroDCENet(keras.Model):
     def summary(self):
         inputs = Input(shape=(256, 256, 3))
         return Model(inputs=inputs, outputs=self.call(inputs)).summary()
+
+
+
+def get_zero_dce(input_shape=(None, None, 3), n_filters=32):
+    input_img = Input(shape=input_shape)
+    
+    conv_1 = Conv2D(n_filters, (3, 3), strides=(1, 1), activation="relu", padding="same", name="conv_1")(input_img)
+    conv_2 = Conv2D(n_filters, (3, 3), strides=(1, 1), activation="relu", padding="same", name="conv_2")(conv_1)
+    
+    conv_3 = Conv2D(n_filters, (3, 3), strides=(1, 1), activation="relu", padding="same", name="conv_3")(conv_2)
+    conv_4 = Conv2D(n_filters, (3, 3), strides=(1, 1), activation="relu", padding="same", name="conv_4")(conv_3)
+    concat_1 = Concatenate(axis=-1)([conv_4, conv_3])
+ 
+    conv_5 = Conv2D(n_filters, (3, 3), strides=(1, 1), activation="relu", padding="same", name="conv_5")(concat_1)
+    concat_2 = Concatenate(axis=-1)([conv_5, conv_2])
+    
+    conv_6 = Conv2D(n_filters, (3, 3), strides=(1, 1), activation="relu", padding="same", name="conv_6")(concat_2)
+    concat_3 = Concatenate(axis=-1)([conv_6, conv_1])
+    
+    A = Conv2D(24, (3, 3), strides=(1, 1), activation="tanh", padding="same", name="curve_params")(concat_3)
+    
+    r1 = A[:,:,:,: 3]
+    r2 = A[:,:,:,3: 6]
+    r3 = A[:,:,:,6: 9]
+    r4 = A[:,:,:,9: 12]
+    r5 = A[:,:,:,12: 15]
+    r6 = A[:,:,:,15: 18]
+    r7 = A[:,:,:,18: 21]
+    r8 = A[:,:,:,21: 24]
+        
+    x = input_img + r1 * (tf.pow(input_img,2)-input_img)
+    x = x + r2 * (tf.pow(x,2)-x)
+    x = x + r3 * (tf.pow(x,2)-x)
+    enhanced_image_1 = x + r4*(tf.pow(x,2)-x)
+    x = enhanced_image_1 + r5*(tf.pow(enhanced_image_1,2)-enhanced_image_1)		
+    x = x + r6*(tf.pow(x,2)-x)	
+    x = x + r7*(tf.pow(x,2)-x)
+    enhanced_image = x + r8*(tf.pow(x,2)-x)
+        
+    return Model(inputs=input_img, outputs=enhanced_image)
