90ad1239a6402852fee2c5fadd3f74dbba82b49d
adminuser <adminuser@kali.mvm>
2023-05-30 08:40:18 +0530

    updated the train.py file

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 class Trainer:
-    def __init__(self, model, optimzer, ckpt, ckpt_manager, summary_writer, epochs):
+    def __init__(self, model, optimizer, ckpt, ckpt_manager, epochs):
         self.model = model 
         self.ckpt = ckpt
         self.ckpt_manager = ckpt_manager
         self.epochs = epochs
-        self.optimzer = optimizer
+        self.optimizer = optimizer
         
         self.spatial_consistency_loss_tracker = keras.metrics.Mean()
         self.color_constancy_loss_tracker = keras.metrics.Mean()
@@ -20,11 +20,14 @@ class Trainer:
         
         self.spatial_consistency_loss_func = SpatialConsistencyLoss()
         self.color_constancy_loss_func = ColorConstancyLoss()
-        self.exposure_loss_func = ExposureLoss()
+        self.exposure_loss_func = ExposureLoss(0.6)
         self.illumination_smoothness_loss_func = IlluminationSmothnessLoss()
         
-        log_dir = 'logs/batch_level/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '/train'
+        log_dir = 'loss/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '/train'
         self.train_writer = tf.summary.create_file_writer(log_dir)
+        
+        log_dir = 'loss/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '/val'
+        self.val_writer = tf.summary.create_file_writer(log_dir)
     
     def train_step(self, image):
         
@@ -37,12 +40,12 @@ class Trainer:
         params = self.model.trainable_weights
         grads = tape.gradient(total_loss, params)
         
-        self.optimizer.apply_gradients(grads, params)
+        self.optimizer.apply_gradients(zip(grads, params))
         
-        self.spatial_consistency_loss_tracker.update_state(spatialcon_loss)
-        self.color_constancy_loss_tracker(color_con_loss)
+        self.spatial_consistency_loss_tracker.update_state(spatial_constancy_loss)
+        self.color_constancy_loss_tracker(color_constancy_loss)
         self.exposure_loss_tracker(exposure_loss)
-        self.illumination_smoothness_loss_tracker(illu_smo_loss)
+        self.illumination_smoothness_loss_tracker(illumination_loss)
         self.total_loss_tracker(total_loss)
         
         loss_dict = {
@@ -61,10 +64,10 @@ class Trainer:
         spatial_constancy_loss, exposure_loss, illumination_loss, color_constancy_loss = loss_res
         total_loss = spatial_constancy_loss + exposure_loss + illumination_loss + color_constancy_loss
         
-        self.val_spatial_consistency_loss_tracker.update_state(spatialcon_loss)
-        self.val_color_constancy_loss_tracker.update_state(color_con_loss)
+        self.val_spatial_consistency_loss_tracker.update_state(spatial_constancy_loss)
+        self.val_color_constancy_loss_tracker.update_state(color_constancy_loss)
         self.val_exposure_loss_tracker.update_state(exposure_loss)
-        self.val_illumination_smoothness_loss_tracker.update_state(illu_smo_loss)
+        self.val_illumination_smoothness_loss_tracker.update_state(illumination_loss)
         self.val_total_loss_tracker.update_state(total_loss)
         
         loss_dict = {
@@ -96,8 +99,15 @@ class Trainer:
     def train(self, train_ds, val_ds):
         history = defaultdict(list)
         
-        for epoch in range(self.epoch): 
-            for step, training_batch in tqdm(enumerate(train_ds), total_len=len(train_ds)): 
+        self.ckpt.restore(self.ckpt_manager.latest_checkpoint)
+        if self.ckpt_manager.latest_checkpoint:
+            print("Restored from {}".format(self.ckpt_manager.latest_checkpoint))
+        else:
+            print("Initializing from scratch.")
+        
+        for epoch in range(self.epochs): 
+            print(f"Epoch: {epoch}: ")
+            for step, training_batch in tqdm(enumerate(train_ds), total=len(train_ds)): 
                 loss_dict = self.train_step(training_batch)
                 train_loss = loss_dict["total_loss"]
                 exposure_loss = loss_dict["exposure_loss"]
@@ -112,30 +122,42 @@ class Trainer:
                 val_color_constancy_loss = loss_dict["color_constancy_loss"]
                 val_illumination_smoothness_loss = loss_dict["illumination_smoothness_loss"]
                 val_spatial_consistency_loss = loss_dict["spatial_consistency_loss"]
-            
-        history["train_loss"].append(train_loss) 
-        history["val_loss"].append(val_loss) 
-        history["exposure_loss"].append(exposure_loss) 
-        history["val_exposure_loss"].append(val_exposure_loss) 
-        history["color_constancy_loss"].append(color_constancy_loss) 
-        history["val_color_constancy_loss"].append(val_color_constancy_loss) 
-        history["illumination_smoothness_loss"].append(val_illumination_smoothness_loss) 
-        history["spatial_consistency_loss"].append(val_loss) 
-        history["val_spatial_consistency_loss"].append(train_loss) 
         
-        with self.train_writer.as_default(step=epoch):
-            tf.summary.scalar('train_loss', train_loss)
-            tf.summary.scalar('val_loss', val_loss)
-            tf.summary.scalar('exposure_loss', train_loss)
-            tf.summary.scalar('val_exposure_loss', val_loss)
-            tf.summary.scalar('color_constancy_loss', train_loss)
-            tf.summary.scalar('val_color_constancy_loss', val_loss)
-            tf.summary.scalar('illumination_smoothness_loss', train_loss)
-            tf.summary.scalar('spatial_consistency_loss', val_loss)
-            tf.summary.scalar('spatial_consistency_loss', train_loss)
-            tf.summary.scalar('val_spatial_consistency_loss', val_loss)
+            self.ckpt.epoch.assign_add(1)
+            history["train_loss"].append(train_loss) 
+            history["val_loss"].append(val_loss) 
+            history["exposure_loss"].append(exposure_loss) 
+            history["val_exposure_loss"].append(val_exposure_loss) 
+            history["color_constancy_loss"].append(color_constancy_loss) 
+            history["val_color_constancy_loss"].append(val_color_constancy_loss) 
+            history["illumination_smoothness_loss"].append(illumination_smoothness_loss) 
+            history["val_illumination_smoothness_loss"].append(val_illumination_smoothness_loss) 
+            history["spatial_consistency_loss"].append(spatial_consistency_loss) 
+            history["val_spatial_consistency_loss"].append(val_spatial_consistency_loss) 
+        
+            with self.train_writer.as_default(step=epoch):
+                tf.summary.scalar('train_loss', train_loss)
+                tf.summary.scalar('exposure_loss', exposure_loss)
+                tf.summary.scalar('color_constancy_loss', color_constancy_loss)
+                tf.summary.scalar('illumination_smoothness_loss', illumination_smoothness_loss)
+                tf.summary.scalar('spatial_consistency_loss', spatial_consistency_loss)
+
+            with self.val_writer.as_default(step=epoch):
+                tf.summary.scalar('val_loss', val_loss)
+                tf.summary.scalar('val_exposure_loss', val_exposure_loss)
+                tf.summary.scalar('val_color_constancy_loss', val_color_constancy_loss)
+                tf.summary.scalar('val_illumination_smoothness_loss', val_illumination_smoothness_loss)
+                tf.summary.scalar('val_spatial_consistency_loss', val_spatial_consistency_loss)
             
-        print(f'epoch: {epoch}, train_loss: {train_loss}, val_loss: {val_loss}, exposure_loss: {exposure_loss}, val_exposure_loss: {val_exposure_loss}')
+            print(f'train_loss: {train_loss}, exposure_loss: {exposure_loss}, color_constancy_loss: {color_constancy_loss}, illumination_smoothness_loss: {illumination_smoothness_loss}, spatial_consistency_loss: {spatial_consistency_loss}\n')
+            print(f'val_loss: {val_loss}, val_exposure_loss: {val_exposure_loss}, val_color_constancy_loss: {val_color_constancy_loss}, val_illumination_smoothness_loss: {val_illumination_smoothness_loss}, val_spatial_consistency_loss: {val_spatial_consistency_loss}\n')
+            
+            if epoch %2 == 0: 
+                save_path = self.ckpt_manager.save()
+                
+                print("Saved checkpoint for step {}: {}".format(int(self.ckpt.epoch), save_path))
+            
+        return self.model
     
     def compute_psnr(self):
         pass 
